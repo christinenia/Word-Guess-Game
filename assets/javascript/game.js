296ac53c942f27624db599ff10088657042bd5ac
Christine Nia <christinenia@me.com>
2018-09-09 22:05:52 -0700

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+var alphabetArray = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
+
+var referenceArray = [];
+
+var wordList = ["awakens", "galaxy", "george", "lucas", "skywalker", "force", "star", "yoda", "kylo ren", "luke", "han solo", "bb8", "droid", "r2d2", "chewbacca", "chewie", "c3po", "lightsaber", "mace windu", "jar jar binks", "anakin", "darth vader", "poe dameron", "general hux", "rey", "finn", "leia", "millenium falcon", "galactic empire", "first order", "jedi", "storm trooper", "boba fett", "clone wars", "count dooku", "obi wan kenobi", "ackbar", "death star"];
+
+
+
