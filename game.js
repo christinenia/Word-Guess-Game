4d479eff5875c755efc1302403846f955daf83ce
Christine Nia <christinenia@me.com>
2018-09-13 14:09:46 -0700

    updates

--- game.js
+++ game.js
@@ -1,13 +1,11 @@
-var alphabetArray = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
-
 // create array of words
-var wordList = ["awakens", "galaxy", "george", "lucas", "skywalker", "force", "star", "yoda", "kylo ren", "luke", "han solo", "bb8", "droid", "r2d2", "chewbacca", "chewie", "c3po", "lightsaber", "mace windu", "jar jar binks", "anakin", "darth vader", "poe dameron", "general hux", "rey", "finn", "leia", "millenium falcon", "galactic empire", "first order", "jedi", "storm trooper", "boba fett", "clone wars", "count dooku", "obi wan kenobi", "ackbar", "death star"];
+var wordList = ["force awakens", "galaxy", "george lucas", "force", "yoda", "kylo ren", "luke skywalker", "han solo", "bb8", "droid", "r2d2", "chewbacca", "chewie", "c3po", "lightsaber", "mace windu", "jar jar binks", "anakin", "darth vader", "poe dameron", "general hux", "rey", "finn", "leia", "millenium falcon", "galactic empire", "first order", "jedi", "storm trooper", "boba fett", "clone wars", "count dooku", "obi wan kenobi", "ackbar", "death star"];
 
 var randomWord = Math.floor(Math.random() * wordList.length);
 var rightWord = [];
 var wrongWord = [];
 var chosenWord = wordList[randomWord];
-console.log(chosenWord);
+// console.log(chosenWord);
 var underScore = [];
 
 var generateUnderscore = () => {
@@ -18,7 +16,9 @@ var generateUnderscore = () => {
 }
 
 console.log(generateUnderscore()); 
+
 //get users guess
+
 document.addEventListener("keypress", (event) => {
     var keyword = String.fromCharCode(event.keyCode);
 //if users guess is correct
@@ -29,3 +29,5 @@ document.addEventListener("keypress", (event) => {
      else wrongWord.push(keyword);
      
 });
+
+
